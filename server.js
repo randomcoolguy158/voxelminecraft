8f43927b6b242e5c2ba7bd5168690f420149fd90
randomcoolguy158 <malekhoss2014@gmail.com>
2026-06-25 20:05:01 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 const fs   = require('fs');
 const path = require('path');
 
-const PORT          = 8080;
+const PORT = process.env.PORT || 8080;
 const SAVE_INTERVAL = 30000;
 const DATA_FILE     = path.join(__dirname, 'world_data.json');
 
